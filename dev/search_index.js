2377b94c57cc408038f84b7cefa717c62ff3571e
Documenter.jl <documenter@juliadocs.github.io>
2022-06-20 12:23:28 +0000

    build based on df6c4e0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RiskBudgeting","category":"page"},{"location":"#RiskBudgeting","page":"Home","title":"RiskBudgeting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RiskBudgeting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RiskBudgeting]","category":"page"}]
+}
