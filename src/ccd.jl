d830021376861b67f7a196c5489cd68f9a0bd8b0
xiar-fatah <xiar.fatah@gmail.com>
2022-07-01 22:52:42 +0200

    Finished the ccd algorithm

--- src/ccd.jl
+++ src/ccd.jl
@@ -1,21 +1,27 @@
+function ccd(cov::AbstractMatrix, b::AbstractVector, 
+    max_iter::Int64 = 10000, tol::Float64 = 10^(-4))::AbstractVector
+# The risk budgeting vector must be positive
+@assert all(b.>0) == true
+# The covariance matrix must be NxN
+@assert (size(cov)[1] == size(cov)[2]) == true
 
+x = 1 ./ diag(cov)
+Σx = cov * x
+xΣx = x' * Σx
+for iter = 1:max_iter
+    i = (iter) % size(cov)[1] + 1
+    nonsqrt = -Σx[i] + x[i]*cov[i,i]
+    x̃ = (nonsqrt + sqrt(nonsqrt^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
+    
+    Σx = Σx + cov[:,i]*(x̃ - x[i]) # Σx̃
+    xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃^2) - 2*x[i]*(cov[i, :]' * x)  # σ(x̃)
+    x[i] = x̃
 
-function ccd(cov::AbstractMatrix, b::AbstractVector, max_iter::Int64, tol::Float64)::AbstractVector
-    x = 1 ./ diag(cov)
-    Σx = cov * x
-    xΣx = x' * Σx
-    for iter = 1:max_iter
-        i = iter % cov.shape[1]
-        nonsqrt = -Σx[i] + x[i]*cov[i,i]
-        x̃ = (nonsqrt + sqrt(nonsqrt^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
-        Σx = Σx + cov[:,i]*(x_new - x[i]) # Σx̃
-        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃^2) - 2*x[i]*(cov[i, :].* x)  # σ(x̃)
-        x[i] = x̃
-        xΣx = xΣx + 2*x[i]* (cov[i,:] .* x)
-        if max(abs(x * (cov .* x) - b)) < tol
-            return x
-        end
+    xΣx = xΣx + 2*x[i]* (cov[i,:]' * x)
+    if maximum(abs.(x .* (cov * x) .- b)) < tol
+        return x
     end
-    println("Cyclical Coordinate Descent has failed to converge!")
-    return x    
+end
+println("Cyclical Coordinate Descent has failed to converge!")
+return x  
 end
