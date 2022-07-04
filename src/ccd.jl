8bc696eef12a2b7fd5a005be2843b7aa20166faa
xiar-fatah <xiar.fatah@gmail.com>
2022-07-04 12:01:55 +0200

    Added the risk budgeting portfolios from Raffinots paper

--- src/ccd.jl
+++ src/ccd.jl
@@ -46,6 +46,32 @@ function ccd(cov::AbstractMatrix, b::AbstractVector{Float64},
     
 end
 
+function ccd(cov::AbstractMatrix, b::AbstractVector{Float64}, 
+    max_iter::Int64, tol::Float64, predefined::Int64)::AbstractVector
+    
+    x = 1 ./ diag(cov)
+    Σx = cov * x
+    xΣx = x' * Σx
+    for iter = 1:max_iter
+        i = (iter) % size(cov)[1] + 1
+        nonsqrt = -Σx[i] + x[i]*cov[i,i]
+        x̃ = (nonsqrt + sqrt(nonsqrt^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
+        
+        Σx = Σx + cov[:,i]*(x̃ - x[i]) # Σx̃
+        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃^2) - 2*x[i]*(cov[i, :]' * x)  # σ(x̃)
+        x[i] = x̃
+
+        xΣx = xΣx + 2*x[i]* (cov[i,:]' * x)
+        if maximum(abs.(x .* (cov * x) .- b)) < tol
+            return x
+        end
+    end
+
+    println("Cyclical Coordinate Descent has failed to converge!")
+    return x  
+    
+end
+
 """
     fastccd(cov, b, [max_iter], [tol])
 
---
+++ src/portfolios.jl
@@ -0,0 +1,43 @@
+
+function mostdiversified(cov::AbstractMatrix, 
+    max_iter::Int64 = 10000, tol::Float64 = 10^(-4))::AbstractVector
+    # The covariance matrix must be NxN
+    @assert (size(cov)[1] == size(cov)[2]) == true
+
+    N = size(cov)[1]
+    σ = sqrt.(diag(cov))
+    b = [1/N for i=1:N]
+    b = (σ .* b) / (σ' * b)
+
+    # The risk budgeting vector must be positive
+    @assert all(b.>0) == true
+
+    return ccd(cov, b, max_iter, tol, predefined = 1)
+end
+
+function equalriskcontribution(cov::AbstractMatrix, 
+    max_iter::Int64 = 10000, tol::Float64 = 10^(-4))::AbstractVector
+    # The covariance matrix must be NxN
+    @assert (size(cov)[1] == size(cov)[2]) == true
+
+    N = size(cov)[1]
+    b = [1/N for i=1:N]
+
+    # The risk budgeting vector must be positive
+    @assert all(b.>0) == true
+    return ccd(cov, b, max_iter, tol, predefined = 1)
+end
+
+function inversevariance(cov::AbstractMatrix, 
+    max_iter::Int64 = 10000, tol::Float64 = 10^(-4))::AbstractVector
+    # The covariance matrix must be NxN
+    @assert (size(cov)[1] == size(cov)[2]) == true
+
+    σ = sqrt.(diag(cov))
+    b = σ.^(-2) ./ sum(σ.^(-2))
+
+    # The risk budgeting vector must be positive
+    @assert all(b.>0) == true
+
+    return ccd(cov, b, max_iter, tol, predefined = 1)
+end
