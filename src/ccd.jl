47fd44dd57124da677f1dc49671c44d29bec99fd
xiar-fatah <xiar.fatah@gmail.com>
2022-07-07 16:16:19 +0200

    Added correct tolerance checking and new initilization

--- src/ccd.jl
+++ src/ccd.jl
@@ -17,58 +17,56 @@ External links
 """
 function ccd(cov::AbstractMatrix, b::AbstractVector{Float64}, 
     max_iter::Int64 = 10000, tol::Float64 = 10^(-4))::AbstractVector
-    
     # The risk budgeting vector must be positive
     @assert all(b.>0) == true
     # The covariance matrix must be NxN
-    @assert (size(cov)[1] == size(cov)[2]) == true
+    @assert (size(cov,1) == size(cov,2)) == true
+    x = 1 ./ sqrt.(diag(cov)) 
 
-    x = 1 ./ diag(cov)
+    x̃ = x
     Σx = cov * x
     xΣx = x' * Σx
     for iter = 1:max_iter
-        i = (iter) % size(cov)[1] + 1
+        i = (iter) % size(cov,1) + 1
         bb = -Σx[i] + x[i]*cov[i,i]
-        x̃ = (bb + sqrt(bb^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
-        
-        Σx = Σx + cov[:,i]*(x̃ - x[i]) # Σx̃
-        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃^2) - 2*x[i]*(cov[i, :]' * x)  # σ(x̃)
-        x[i] = x̃
-
-        xΣx = xΣx + 2*x[i]* (cov[i,:]' * x)
-        if maximum(abs.(x .* (cov * x) .- b)) < tol
-            return x
+        x̃[i] = (bb + sqrt(bb^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
+        Σx = Σx + cov[:,i]*(x̃[i] - x[i]) # Σx̃
+        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃[i]^2) - 2*x[i]*(cov[i, :]' * x) + 2*x̃[i] * (cov[i,:]' * x̃)  # σ(x̃)
+        rc = (x̃ .* (cov * x̃)) ./ (sqrt(x̃' * (cov * x̃)))
+        if maximum(abs.(rc / sum(rc) .- b)) < tol
+            return x̃ / sum(x)
         end
-    end
+        x = x̃
 
+    end
     println("Cyclical Coordinate Descent has failed to converge!")
-    return x  
-    
+    return x̃ / sum(x)
 end
+
 
 function ccd(cov::AbstractMatrix, b::AbstractVector{Float64}, 
     max_iter::Int64, tol::Float64, predefined::Int64)::AbstractVector
     
-    x = 1 ./ diag(cov)
+    x = 1 ./ sqrt.(diag(cov)) 
+
+    x̃ = x
     Σx = cov * x
     xΣx = x' * Σx
     for iter = 1:max_iter
-        i = (iter) % size(cov)[1] + 1
+        i = (iter) % size(cov,1) + 1
         bb = -Σx[i] + x[i]*cov[i,i]
-        x̃ = (bb + sqrt(bb^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
-        
-        Σx = Σx + cov[:,i]*(x̃ - x[i]) # Σx̃
-        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃^2) - 2*x[i]*(cov[i, :]' * x)  # σ(x̃)
-        x[i] = x̃
-
-        xΣx = xΣx + 2*x[i]* (cov[i,:]' * x)
-        if maximum(abs.(x .* (cov * x) .- b)) < tol
-            return x
+        x̃[i] = (bb + sqrt(bb^2 + 4*cov[i,i] * b[i] * sqrt(xΣx))) / (2*cov[i, i])
+        Σx = Σx + cov[:,i]*(x̃[i] - x[i]) # Σx̃
+        xΣx = xΣx + cov[i,i] * (x[i]^2 - x̃[i]^2) - 2*x[i]*(cov[i, :]' * x) + 2*x̃[i] * (cov[i,:]' * x̃)  # σ(x̃)
+        rc = (x̃ .* (cov * x̃)) ./ (sqrt(x̃' * (cov * x̃)))
+        if maximum(abs.(rc / sum(rc) .- b)) < tol
+            return x̃ / sum(x)
         end
-    end
+        x = x̃
 
+    end
     println("Cyclical Coordinate Descent has failed to converge!")
-    return x  
+    return x̃ / sum(x)
     
 end
 
