b923cf8f560cab1b684bd6a31e10c2cb4c837b77
xiar-fatah <xiar.fatah@gmail.com>
2022-07-17 14:00:00 +0200

    Fixed old tests

--- test/ccd.jl
+++ test/ccd.jl
@@ -4,12 +4,12 @@
         0.5 1 0.5
         0.5 0.5 1]
 
-    @test ccd(cov_test, b_test) ≈ [0.30618621784789835 for i=1:3]
+    @test ccd(cov_test, b_test) ≈ [1/3 for i=1:3]
 
-    b_test = [0.33 for i=1:3]
-    @test ccd(cov_test, b_test) ≈ [0.404165807559224 for i=1:3]
+    b_test = [1/3 for i=1:3]
+    @test ccd(cov_test, b_test) ≈ [1/3 for i=1:3]
 
-    b_minus = [-0.33 for i = 1:3]
+    b_minus = [-1/3 for i = 1:3]
     @test_throws AssertionError ccd(cov_test, b_minus)
 
     cov_non_square = [1 0.5
@@ -18,4 +18,6 @@
     @test_throws AssertionError ccd(cov_non_square, b_test)
 
 
-end 
+
+
+end
